561a11e8541009597bf8301747b08b228287045a
vitali <pedalka@gmail.com>
2019-06-22 17:52:54 +0300

    rename fun and param

--- src/arrays/RotateArray.java
+++ src/arrays/RotateArray.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class RotateArray {
 
 
-    public static List<Integer> rotateArray(List<Integer> arr, int rotations) {
+    public static List<Integer> rotateArrayByRotationsIndex(List<Integer> arr, int rotationsIndex) {
         int length = arr.size();
-        rotations = rotations % length;
-        if(rotations < 0) {
-            rotations = rotations+length;
+        rotationsIndex = rotationsIndex % length;
+        if(rotationsIndex < 0) {
+            rotationsIndex = rotationsIndex + length;
         }
 
         /*
@@ -19,7 +19,7 @@ public class RotateArray {
          * For eg: if rotation = 2, the copy last 2 values into temp array
          * */
         List<Integer> tempList = new ArrayList<>();
-        for(int i=(length-rotations); i<length; i++) {
+        for(int i=(length - rotationsIndex); i<length; i++) {
             tempList.add(arr.get(i));
         }
 
@@ -28,11 +28,11 @@ public class RotateArray {
          * Step 2: shift all values not included in the rotation to the end of the array.
          * For eg: if rotation = 7, then shift length-rotation i.e 10-7 = 3 values to the end of the array
          * */
-        for(int i=length-1; i>= rotations; i--) {
-            arr.set(i, arr.get(i-rotations));
+        for(int i=length-1; i>= rotationsIndex; i--) {
+            arr.set(i, arr.get(i-rotationsIndex));
         }
 
-        for(int i=0; i<rotations; i++) {
+        for(int i=0; i<rotationsIndex; i++) {
             arr.set(i, tempList.get(i));
         }
 
@@ -41,6 +41,6 @@ public class RotateArray {
 
     public static void main(String[] args) {
         Integer[] arr1 = {1, 2, 3, 4, 5};
-        System.out.println(rotateArray(Arrays.asList(arr1), 2));
+        System.out.println(rotateArrayByRotationsIndex(Arrays.asList(arr1), 2));
     }
 }
